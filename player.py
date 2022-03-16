18908c13ecc85984c1ee923b95c1a55020cc3baf
gheceam <gheceam@san.rr.com>
2022-03-15 22:21:07 -0700

    modified string returned in move() method

---
+++ player.py
@@ -0,0 +1,15 @@
+"""The Player class is the parent class for all of the Players
+in this game"""
+
+
+class Player:
+    def move(self):
+        return 'rocks'
+
+    def learn(self, my_move, their_move):
+        pass
+
+
+class RandomPlayer(Player):
+    def move():
+        pass
